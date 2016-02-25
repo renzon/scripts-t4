122e4f8a525275e99c03b8f4f90355afd409c304
renzon <renzon@gmail.com>
2016-02-25 08:13:10 -0300

    Inicio programacao funcional, alunos ficando malucos com conceito

---
+++ aula5/funcional.js
@@ -0,0 +1,31 @@
+
+
+
+
+var f=function f(func){
+  console.log('Executando f');
+}
+
+console.log(f);
+var g=f;
+f=1;
+console.log(g);
+g();
+
+function h(func){
+  function nova(x){
+    return func(x+1);
+  }
+
+  return nova;
+}
+
+function reta(x){
+  return x;
+}
+
+var retaComShift=h(reta);
+
+console.log(retaComShift);
+console.log(retaComShift(1));
+
