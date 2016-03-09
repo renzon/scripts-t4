4ec0e2b84b2f2ab6d15aeb4e0c138aad441f45f4
renzon <renzon@gmail.com>
2016-03-08 22:18:48 -0300

    Aula sobre herança em JS

---
+++ aula6/manada.js
@@ -0,0 +1,14 @@
+var manadaVirgula = new ManadaVirgula();
+var manadaSustenidaDupla = new ManadaSustenido();
+var animas = [new Cachorro(), new Gato()];
+
+animas.forEach(function (animal) {
+  manadaVirgula.adicionar(animal);
+  manadaSustenido.adicionar(animal);
+});
+
+// Print Esperado: Au, Miau
+console.log(manadaVirgula.barulhos());
+
+// Print Esperado: Au, Au, Miau, Miau
+console.log(manadaSustenidaDupla.barulhos());
